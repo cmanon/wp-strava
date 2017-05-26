a8c40c62c4c108d173804ac394438e8f790910eb
Justin Foell <jrfoell@users.noreply.github.com>
2017-05-26 11:07:28 -0500

    Removed old create messages script in favor of i18n tools makepot.php from here: http://develop.svn.wordpress.org/trunk/tools/i18n/

--- lang/create_messages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/usr/bin/xgettext --language=PHP --indent --keyword=__ --keyword=_e --keyword=__ngettext:1,2 -s -n --from-code=UTF8 \
-	../wp-strava.php \
-	../readme.txt
-
-msgfmt -o wp-strava-es_ES.mo wp-strava-es_ES.po
