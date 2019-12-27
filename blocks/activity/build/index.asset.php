93bc67e1fc71eb667c36ba1c6305cd826ebcacdd
Justin Foell <630830+jrfoell@users.noreply.github.com>
2019-12-27 16:09:28 -0600

    Added example render callback Prefixed function names

--- blocks/activity/build/index.asset.php
+++ blocks/activity/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '75cbab21f59424fbb0a36e9cf02ba1af');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '70b7abdf6184e1241f188c3eb90bb933');
--- blocks/activity/src/index.js
+++ blocks/activity/src/index.js
@@ -2,7 +2,7 @@ import { registerBlockType } from '@wordpress/blocks';
 
 registerBlockType( 'wp-strava/activity', {
     title: 'Strava Activity',
-    icon: 'smiley',
+    icon: 'chart-line',
     category: 'widgets',
     edit: () => <div>Hola, mundo!</div>,
     save: () => <div>Hola, mundo!</div>,
